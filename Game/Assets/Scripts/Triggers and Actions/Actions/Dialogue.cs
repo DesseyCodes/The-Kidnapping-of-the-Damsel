ae9a8b205f162fc0bafb150c105a9b7dff18869c
lightstream <lucas.mar@hotmail.com.br>
2022-11-09 23:53:15 -0300

    Updating Dialogue.cs

--- Game/Assets/Scripts/Triggers and Actions/Actions/Dialogue.cs
+++ Game/Assets/Scripts/Triggers and Actions/Actions/Dialogue.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 
 public class Dialogue : Action
 {
-    //Contains messages.
-    //Types each message.
-    //Waits for player input at the end of each message.
-
-    [SerializeField] Image dialoguePanel;
-    [SerializeField] TMP_Text dialogueText;
+    // Contains messages.
+    // Types each message.
+    // Waits for player input at the end of each message.
+    
     [SerializeField] string[] messages;
     [Tooltip ("Interval between each letter.")]
     [SerializeField] float typingInterval;
     [SerializeField] string continueButton;
 
+    Image dialoguePanel;
+    TMP_Text dialogueText;
+
+    void Awake()
+    {
+        dialoguePanel = GameObject.Find("dialogue panel").GetComponent<Image>();
+        dialogueText = GameObject.Find("dialogue text").GetComponent<TMP_Text>();
+    }
     void OnEnable()
     {
-        //Calling SetActive(true) in the dialogue panel causes a small hiccup. So I'm changing the alpha instead;
+        // Calling a dialogue panel's SetActive(true) causes a hiccup. So I'm changing the alpha instead.
         Color c = dialoguePanel.color;
         c.a = 0.42f;
         dialoguePanel.color = c;
@@ -30,14 +36,14 @@ public class Dialogue : Action
 
     IEnumerator ContinueDialogue()
     {
-        //Show first message.
+        // Show first message.
         for(int i = 0; i < messages[0].Length; i++)
         {
             dialogueText.text += messages[0][i];
             yield return new WaitForSeconds(typingInterval);
         }
 
-        //Show all other messages.
+        // Show all other messages.
         int messageIndex = 1;
 
         while(messageIndex < messages.Length)
@@ -60,7 +66,7 @@ public class Dialogue : Action
             }
         }
 
-        //Require input to end dialogue.
+        // Require input to end dialogue.
         bool canEnd = false;
 
         while(!canEnd)
@@ -72,7 +78,6 @@ public class Dialogue : Action
                 Color c = dialoguePanel.color;
                 c.a = 0;
                 dialoguePanel.color = c;
-
                 dialogueText.text = "";
 
                 canEnd = true;
