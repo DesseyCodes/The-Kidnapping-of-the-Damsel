e485cea06c9a2a2d5024ab4076a8ae7b1ebe01eb
FunnyBear38 <76536028+DesseyCodes@users.noreply.github.com>
2022-11-21 16:31:04 -0500

    NPC Movement
    NPC moves forward. When it collides with an environmental tile, it turns around and walks in the opposite direction. This infinitely repeats.

--- Game/Assets/Scripts/Player and Rock/NPCMovement.cs
+++ Game/Assets/Scripts/Player and Rock/NPCMovement.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
-    [SerializeField] Vector2 minPosition, maxPosition;
+   // [SerializeField] Vector2 minPosition, maxPosition;
 
     private Rigidbody2D npcRB;
 
     public float mSpeed;
 
-    public Vector2 randomPosition;
+    public Vector2 walkDir;
+
+    public Transform walkdir1;
+
+    public BoxCollider2D bCollider;
+
+    //public Vector2 randomPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-         randomPosition = new Vector2(
-            Random.Range(minPosition.x, maxPosition.x),
-            Random.Range(minPosition.y, maxPosition.y));
+         //randomPosition = new Vector2(
+           // Random.Range(minPosition.x, maxPosition.x),
+           // Random.Range(minPosition.y, maxPosition.y));
 
         npcRB = GetComponent<Rigidbody2D>();
+
+        Physics2D.IgnoreLayerCollision(3, 11);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        npcRB.velocity = npcRB.velocity.normalized * mSpeed;
-        npcRB.AddForce(randomPosition.normalized * mSpeed * Time.deltaTime);
+        npcRB.velocity = new Vector2(walkdir1.transform.position.x, walkdir1.transform.position.y) * mSpeed * Time.deltaTime;
+        npcRB.AddForce(walkdir1.transform.position * mSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Environmental"))
+        {
+            //transform.Rotate(new Vector2(0, 180));
+            Debug.Log("We hit an environmental object!");
+            //npcRB.MoveRotation(npcRB.rotation * 180);
+            transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            mSpeed *= -1;
+        }
     }
 }
